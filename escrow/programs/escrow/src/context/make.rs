fa55495d9cfd80a0ddae66a0f183441adf478298
Santhosh <sansii1997@gmail.com>
2025-01-24 23:57:34 +0100

    updated escrow code with test cases

--- escrow/programs/escrow/src/context/make.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[derive(Accounts)]
-pub struct Make<'info> {
-    #[account(mut)]
-    maker: Signer<'info>,
-    // #[account(mut)]
-    // maker: Account<'info>,
-}
